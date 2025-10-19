354c2adbef8493583e68fb96487be1e8a6aa7fec
ismaelucky342 <ismaelhc342@gmail.com>
2025-10-19 22:15:55 +0200

    madre mia q esto pase el examen

--- Exam_Rank_04/Level_2/argo/argo.c
+++ Exam_Rank_04/Level_2/argo/argo.c
@@ -3,32 +3,39 @@
 #include <ctype.h>
 #include <string.h>
 
-typedef struct pair { char *key; struct json value; } pair;
-typedef struct json {
-    enum { MAP, INTEGER, STRING } type;
-    union { struct { pair *data; size_t size; } map; int integer; char *string; };
-} json;
+struct pair; struct json;
+typedef struct json{enum{MAP,INTEGER,STRING}type;union{struct{struct pair*data;size_t size;}map;int integer;char*string;};}json;
+typedef struct pair{char*key;json value;}pair;
 
-int peek(FILE *s){int c=getc(s);ungetc(c,s);return c;}
-void unexpected(FILE *s){printf("unexpected %s\n", peek(s)!=EOF?(char[]){'\'',peek(s),0}:"end of input");}
-int accept(FILE*s,char c){return peek(s)==c?(void)getc(s),1:0;}
+int peek(FILE*s){int c=getc(s);return ungetc(c,s),c;}
+void unexpected(FILE*s){int c=peek(s);printf("unexpected %s\n",c==EOF?"end of input":(char[]){'t','o','k','e','n',' ','\'',c,'\'',0});}
+int accept(FILE*s,char c){return peek(s)==c?(getc(s),1):0;}
 int expect(FILE*s,char c){return accept(s,c)?1:(unexpected(s),0);}
-
 int parse_int(json*d,FILE*s){int n;return fscanf(s,"%d",&n)==1?(d->type=INTEGER,d->integer=n,1):(unexpected(s),-1);}
-int parse_string(json*d,FILE*s){char buf[4096],c;int i=0;if(!expect(s,'"'))return-1;while((c=getc(s))!='"'){if(c==EOF)return unexpected(s),-1;if(c=='\\')c=getc(s);buf[i++]=c;}buf[i]=0;return d->type=STRING,d->string=strdup(buf),1;}
+int parse_string(json*d,FILE*s){char b[4096],c;int i=0;if(!expect(s,'"'))return-1;while((c=getc(s))!=EOF){if(c=='"')return b[i]=0,d->type=STRING,d->string=strdup(b),1;if(c=='\\'&&(c=getc(s))==EOF)break;b[i++]=c;}unexpected(s);return-1;}
 
-int parser(json*d,FILE*s); // forward
-int parse_map(json*d,FILE*s){pair*it=NULL;size_t n=0;json key;if(!expect(s,'{'))return-1;while(!accept(s,'}')){it=realloc(it,sizeof(pair)*(n+1));if(parse_string(&key,s)==-1){free(it);return-1;}if(!expect(s,':')){free(key.string);free(it);return-1;}if(parser(&it[n].value,s)==-1){free(key.string);free(it);return-1;}it[n].key=key.string;n++;if(!accept(s,',') && peek(s)!='}') return unexpected(s),free(it),-1;}return d->type=MAP,d->map.size=n,d->map.data=it,1;}
-
-int parser(json*d,FILE*s){int c=peek(s);return c=='"'?parse_string(d,s):isdigit(c)||c=='-'?parse_int(d,s):c=='{'?parse_map(d,s):(unexpected(s),-1);}
+int parser(json*d,FILE*s);
+int parse_map(json*d,FILE*s){
+    pair*it=NULL;size_t n=0;json key;
+    if(!expect(s,'{'))return-1;
+    while(!accept(s,'}')){
+        it=realloc(it,sizeof(pair)*(n+1));
+        if(parse_string(&key,s)==-1)return free(it),-1;
+        if(!expect(s,':'))return free(key.string),free(it),-1;
+        if(peek(s)=='}'){unexpected(s);free(key.string);free(it);return-1;}
+        if(parser(&it[n].value,s)==-1){free(key.string);free(it);return-1;}
+        it[n].key=key.string;n++;
+        if(!accept(s,',')&&peek(s)!='}')return unexpected(s),free(it),-1;
+    }
+    return d->type=MAP,d->map.size=n,d->map.data=it,1;
+}
+int parser(json*d,FILE*s){int c=peek(s);return c==EOF?(unexpected(s),-1):c=='"'?parse_string(d,s):isdigit(c)||c=='-'?parse_int(d,s):c=='{'?parse_map(d,s):(unexpected(s),-1);}
 int argo(json*d,FILE*s){return parser(d,s);}
 
-void free_json(json j){switch(j.type){case MAP:for(size_t i=0;i<j.map.size;i++){free(j.map.data[i].key);free_json(j.map.data[i].value);}free(j.map.data);break;case STRING:free(j.string);break;default:break;}}
-void serialize(json j){switch(j.type){case INTEGER:printf("%d",j.integer);break;case STRING:putchar('"');for(int i=0;j.string[i];i++){if(j.string[i]=='"'||j.string[i]=='\\')putchar('\\');putchar(j.string[i]);}putchar('"');break;case MAP:putchar('{');for(size_t i=0;i<j.map.size;i++){if(i)putchar(',');serialize((json){.type=STRING,.string=j.map.data[i].key});putchar(':');serialize(j.map.data[i].value);}putchar('}');break;}}
-
-int main(int argc,char **argv){
-    if(argc!=2)return 1;
-    FILE *s=fopen(argv[1],"r");if(!s)return 1;
-    json j;if(argo(&j,s)!=1){free_json(j);fclose(s);return 1;}
-    serialize(j);printf("\n");free_json(j);fclose(s);
+void free_json(json j){if(j.type==MAP){for(size_t i=0;i<j.map.size;i++)free(j.map.data[i].key),free_json(j.map.data[i].value);free(j.map.data);}else if(j.type==STRING)free(j.string);}
+void serialize(json j){
+    j.type==INTEGER?printf("%d",j.integer):
+    j.type==STRING?(putchar('"'),({for(int i=0;j.string[i];i++){if(j.string[i]=='"'||j.string[i]=='\\')putchar('\\');putchar(j.string[i]);}putchar('"');}),0):
+    ({putchar('{');for(size_t i=0;i<j.map.size;i++){if(i)putchar(',');serialize((json){.type=STRING,.string=j.map.data[i].key});putchar(':');serialize(j.map.data[i].value);}putchar('}');});
 }
+int main(int c,char**v){if(c!=2)return 1;FILE*s=fopen(v[1],"r");if(!s)return 1;json j;if(argo(&j,s)!=1)return fclose(s),1;serialize(j);puts("");free_json(j);fclose(s);}
--- Exam_Rank_04/Level_2/vbc/vbc.c
+++ Exam_Rank_04/Level_2/vbc/vbc.c
@@ -2,29 +2,45 @@
 
 char *s;
 
-void unexpected(char c) { printf("Unexpected %s\n", c ? (char[]){'\'', c, '\0'} : "end of file"); }
+void unexpected(char c) {
+    c ? printf("Unexpected token '%c'\n", c) : printf("Unexpected end of file\n");
+}
 
-int ft_factor() { return isdigit(*s) ? *s++ - '0' : (*s == '(' ? (s++, ft_sum(), s++, 0) : 0); }
+int ft_sum(void);
 
-int ft_product() { int a = ft_factor(), b; while(*s == '*') s++, a *= (b = ft_factor()); return a; }
+int ft_factor() {
+    if (isdigit(*s)) return *s++ - '0';
+    if (*s == '(') { s++; int val = ft_sum(); (*s == ')') ? s++ : 0; return val; }
+    return 0;
+}
 
-int ft_sum() { int a = ft_product(), b; while(*s == '+') s++, a += (b = ft_product()); return a; }
+int ft_product() {
+    int a = ft_factor();
+    while (*s == '*') { s++; a *= ft_factor(); }
+    return a;
+}
+
+int ft_sum() {
+    int a = ft_product();
+    while (*s == '+') { s++; a += ft_product(); }
+    return a;
+}
 
 int check_input(char *str) {
     int par = 0, i = 0; char last = 0;
-    while(str[i]) {
+    while (str[i]) {
         par += (str[i] == '(') - (str[i] == ')');
-        if(!isdigit(str[i]) && str[i] != '+' && str[i] != '*' && str[i] != '(' && str[i] != ')')
+        if (!isdigit(str[i]) && str[i] != '+' && str[i] != '*' && str[i] != '(' && str[i] != ')')
             return unexpected(str[i]), 1;
-        if(isdigit(str[i]) && isdigit(str[i+1]))
+        if (isdigit(str[i]) && isdigit(str[i+1]))
             return unexpected(str[i+1]), 1;
         last = str[i++];
     }
-    return par ? unexpected(par>0?'(':')'), 1 : last=='+'||last=='*'?unexpected(0),1:0;
+    if (par) return unexpected(par > 0 ? '(' : ')'), 1;
+    if (last == '+' || last == '*') return unexpected(0), 1;
+    return 0;
 }
 
 int main(int argc, char **argv) {
-    if(argc!=2 || check_input(argv[1])) return 1;
-    s = argv[1];
-    printf("%d\n", ft_sum());
+    return (argc != 2 || check_input(argv[1])) ? 1 : (s = argv[1], printf("%d\n", ft_sum()), 0);
 }
